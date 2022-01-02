3b37b844c625aad1132601adf7bb72341eff636e
zwarunek <zacharywarunek8@gmail.com>
2022-01-02 12:49:46 -0500

    Initial Commit

---
+++ src/main/java/com/zacharywarunek/financialfuture/FinancialFutureBackendApplication.java
@@ -0,0 +1,13 @@
+package com.zacharywarunek.financialfuture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFutureBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FinancialFutureBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/zacharywarunek/financialfuture/FinancialFutureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zacharywarunek.financialfuture;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancialFutureBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
